9c95fafaa167e27af359bff5ae00ff079e6c9988
Aryan Koshti <aryankosti120@gmail.com>
2026-04-03 23:00:29 +0530

    Remove Dulicates from sorted array

---
+++ Arrays/Foundation/RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package Arrays.Foundation;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int nums[] = {0,0,1,1,1,2,2,3,3,4};
+
+        int i = 0;
+        for(int j=1; j<nums.length; j++) {
+            if(nums[j] != nums[i]) {
+                nums[i+1] = nums[j];
+                i++;
+            }     
+        }
+          
+       for (int j = 0; j <= i; j++) {
+            System.out.print(nums[j] + " ");
+       }
+    }
+}
+
